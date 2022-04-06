1e10c7049ad028a0f5d71a94a3e0d22f0e045002
Eugen Kovtun <yevhen.kovtun@yalantis.net>
2022-04-06 11:12:27 +0300

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module amount2wordUA
+
+go 1.16
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"amount2wordUA/amount2word"
+	"fmt"
+)
+
+func main() {
+	fmt.Println(amount2word.RuMoneyUA(123659.54, true))
+}
